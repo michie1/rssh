b9c476781fe0b3135bed93fbc955ad55292cb160
Michiel <>
2015-10-16 08:51:30 +0200

    cd into the same dir on the ssh server

--- rssh.bash
+++ rssh.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 function rssh() {
 	ID="$(preserve -np 1 -t 01:00:00 | grep -oP '[0-9]{4}')"
-	ssh $(preserve -llist | grep $ID | grep -oP 'node[0-9]{3}')
+	ssh $(preserve -llist | grep $ID | grep -oP 'node[0-9]{3}') -t "cd $(pwd); bash"
 }
