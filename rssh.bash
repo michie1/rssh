0fc63e8b88aa7b15f7a0593f3a0ac0a3252ed48c
Michiel <>
2015-11-20 17:24:10 +0100

    Amount of hours as argument

--- rssh.bash
+++ rssh.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 function rssh() {
-	ID="$(preserve -np 1 -t 01:00:00 | grep -oP '[0-9]{4}')"
+	if [ -z "$1" ]
+	then
+		HOURS="1"
+	else
+		HOURS=$1
+	fi
+	ID="$(preserve -np 1 -t 0$HOURS:00:00 | grep -oP '[0-9]{4}')"
 	ssh $(preserve -llist | grep $ID | grep -oP 'node[0-9]{3}') -t "cd $(pwd); bash"
 }
